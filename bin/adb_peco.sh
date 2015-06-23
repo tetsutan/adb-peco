e1019e3e07ea14deb74c17576c2cc134fb56b436
Tomonori Yano <yano@eckrel.com>
2015-06-23 22:43:34 +0900

    add Device name cache

--- bin/adb_peco.sh
+++ bin/adb_peco.sh
@@ -12,25 +12,43 @@ if [ $# -eq 1 ]; then
     fi
 fi
 
-count=`adb devices | sed '/^$/d' | wc -l`
-
-if [ $count -eq 1 ]; then
-	echo "device not found"
-	exit
-fi
-
-if [ $count -eq 2 ]; then
-	device=`adb devices | sed -e "1,1d"`
-else
-	device=`adb devices | sed -e "1,1d" | peco`
-fi
-
-
-if [ "$device" = "" ]; then
+if [ $# -eq 2 ]; then
+  if [ $1 = "adb" -a $2 = "devices" ]; then
+    $1 $2
     exit
+  fi
 fi
 
-IFS="	" read -r id state <<< "$device"
+if [ "$USE_CACHE" = "1" ]; then
+  id=`cat ~/.adbp_last_device`
+fi
+
+if [ "$id" = "" ]; then
+  count=`adb devices | sed '/^$/d' | wc -l`
+
+  if [ $count -eq 1 ]; then
+    echo "device not found"
+    exit
+  fi
+
+  if [ $count -eq 2 ]; then
+    device=`adb devices | sed -e "1,1d"`
+  else
+    device=`adb devices | sed -e "1,1d" | peco`
+  fi
+
+
+  if [ "$device" = "" ]; then
+      exit
+  fi
+
+  IFS="	" read -r id state <<< "$device"
+
+fi
+
+if [ "$USE_CACHE" = "1" ]; then
+  echo $id > ~/.adbp_last_device
+fi
 
 echo "$1 -s $id ${@:2}"
 $1 -s $id ${@:2}
